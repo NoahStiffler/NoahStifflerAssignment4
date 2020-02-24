233324c9bac1c8a5625c4cc29b938b52ebf92847
Noah Stiffler <stifflernoah@gmail.com>
2020-02-24 10:04:24 -0500

    Initial Commit

---
+++ NoahStifflerAssignment4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoahStifflerAssignment4
+//
+//  Created by Noah Stiffler on 2/24/20.
+//  Copyright © 2020 Noah Stiffler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
